58b4d37f17ef6ab6a832816b670deeca4fdc5ff7
fandi prasetyo <fprasetyo4@gmail.com>
2022-09-10 13:30:57 +0700

    try fix some bugs

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,8 +45,6 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: 'http://52.77.253.103:3000/api',
-    proxyHeaders: false,
-    credentials: false
   },
 
   auth: {
